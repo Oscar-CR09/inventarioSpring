cac094c4aa728a16a9aec2c2b23acbd960bb5f33
Oscar <oscarcr189@outlook.com>
2023-11-14 20:26:46 -0600

    Proyecto inventario-2 configuracion y creacion de base de datos

--- src/main/java/cervantes/inventario/InventarioApplication.java
+++ src/main/java/cervantes/inventario/InventarioApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InventarioApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(InventarioApplication.class, args);
+
+
 	}
 
 }
