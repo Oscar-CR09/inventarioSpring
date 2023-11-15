e568a4de08e9e219c9fa73a0b5af57a96f6733e9
Oscar <oscarcr189@outlook.com>
2023-11-14 20:17:01 -0600

    Proyecto inventario-1 creacion de proyecto

---
+++ src/main/java/cervantes/inventario/InventarioApplication.java
@@ -0,0 +1,13 @@
+package cervantes.inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioApplication.class, args);
+	}
+
+}
