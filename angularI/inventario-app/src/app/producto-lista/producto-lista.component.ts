1df2fa552540b7f712b1c5a8741b9325fe09f9dc
Oscar <oscarcr189@outlook.com>
2023-11-17 20:29:43 -0600

    creacion de listado componente

---
+++ angularI/inventario-app/src/app/producto-lista/producto-lista.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producto-lista',
+  templateUrl: './producto-lista.component.html'
+})
+export class ProductoListaComponent {
+
+}
